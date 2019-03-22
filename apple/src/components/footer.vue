43827e4356c0b1d21627c69353024366281f0456
SC-201811221724\lanou <642784739@qq.com>
2019-03-22 16:46:41 +0800

    footer

---
+++ apple/src/components/footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <section class="footer">
+      <p>this is footer</p>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "footer"
+    }
+</script>
+
+<style scoped>
+.footer{
+  width: 100%;
+  height: 20px;
+  background-color: cadetblue;
+  text-align: center;
+}
+</style>
