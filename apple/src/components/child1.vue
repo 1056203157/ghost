e664121e01425dd26fcc63b56f5a815c137d0a50
SC-201811221724\lanou <642784739@qq.com>
2019-03-22 17:25:25 +0800

    新建两个子组件

---
+++ apple/src/components/child1.vue
@@ -0,0 +1,22 @@
+<template>
+    <fieldset>
+      <legend><h3>这是child1组件</h3></legend>
+      <div>
+        宝:掐我脖儿,锁我喉是吧?<br>
+        阳:咋地?<br>
+        宝:臭不要脸的!你给我松开<br>
+        阳:大爷,松开可以,你可别动<br>
+        宝:不动?不可能!一动不动是王八!<br>
+      </div>
+    </fieldset>
+</template>
+
+<script>
+    export default {
+        name: "child1"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ apple/src/components/child2.vue
@@ -0,0 +1,18 @@
+<template>
+  <fieldset>
+    <legend><h3>这是child2组件</h3></legend>
+    <div>
+      <h2>宝哥好厉害!</h2>
+    </div>
+  </fieldset>
+</template>
+
+<script>
+    export default {
+        name: "child2"
+    }
+</script>
+
+<style scoped>
+
+</style>
